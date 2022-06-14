b801f7d6517a73f00162059140ab0783aebd074d
Minh <minhtbse151276@fpt.edu.vn>
2022-06-14 16:39:37 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +40,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
